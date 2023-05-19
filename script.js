1cea25edf4a348bb8a976979f1e8322041c03236
barre <barrenac@oregonstate.edu>
2023-05-18 20:54:06 -0700

    Forgot to save script.js changes from previous push

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ var lowerCaseChar = ("abcdefghijklmnopqrstuvwxyz").split("");
 var upperCaseChar = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("");
 var numChar = ("0123456789").split("");
 var specialChar = ("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~!#$%&'()*+,-./:;<=>?@[\]^_`{|}~").split("");
-var charactersIncluded = [];
-var pushChar = [];
-var newPassword = "";
+
+
 
 
 // This function generates password via prompts and conditionals.
 function generatePassword() {
+  var charactersIncluded = [];
+  var pushChar = [];
+  var newPassword = "";
 
   // Variable holds value of initial prompt asking for amount of characters
   var charAmount = window.prompt("How many characters would you like your password to have?");
@@ -19,25 +21,26 @@ function generatePassword() {
   // Conditional statement prompts user with additional password critertias depending on whether the value of the variable above fits the character length criteria
   if ((charAmount >= 8) && (charAmount <= 128)) {
     
-    // if prompt condition is true, then the lowercase array will be added to empty array
+    // if prompt condition is true, then the lowercase array will be added to empty array and a random character from the lowercase array will be pushed into pushcar array to ensure at least one of the lowercase characters is included in generated password
   if (window.confirm("Click 'OK' if you want to include lowercase characters.")){
     charactersIncluded = charactersIncluded.concat(lowerCaseChar);
+    pushChar.push(lowerCaseChar[Math.floor(Math.random() * lowerCaseChar.length)]);
   }
 
-    // if prompt condition is true, then the uppercase array will be added to empty array
+    // same as condition above, but will push a uppercase char to ensure it is included in generated password
   if (window.confirm("Click 'OK' if you want to include uppercase characters.")){
     charactersIncluded = charactersIncluded.concat(upperCaseChar);
-  }
+    pushChar.push(upperCaseChar[Math.floor(Math.random() * upperCaseChar.length)]);  }
 
-    // if prompt condition is true, then the number array will be added to empty array
+    // same as condition above, but will push a numerical char to ensure it is included in generated password
   if (window.confirm("Click 'OK' if you want to include numbers.")){
     charactersIncluded = charactersIncluded.concat(numChar);
-  }
+    pushChar.push(numChar[Math.floor(Math.random() * numChar.length)]);  }
 
-    // if prompt condition is true, then the special character array will be added to empty array
+    // same as condition above, but will push a special char to ensure it is included in generated password
   if (window.confirm("Click 'OK' if you want to include special characters.")){
     charactersIncluded = charactersIncluded.concat(specialChar);
-  }
+    pushChar.push(specialChar[Math.floor(Math.random() * specialChar.length)]);  }
 
     // if character length condition is not met, then the alert message below will be displayed
   } else {
@@ -45,11 +48,13 @@ function generatePassword() {
   }
 
   // for loop generates random character depending on character amount inputted, pushes random char into array, and turns array to string without commas by use of join()
-  for (var i = 0; i < charAmount; i++) {
+  var adjustedCharAmnt = charAmount - pushChar.length;
+  for (var i = 0; i < adjustedCharAmnt; i++) {
       var index = Math.floor(Math.random() * charactersIncluded.length);
       pushChar.push(charactersIncluded[index]);
-      newPassword = pushChar.join("");
+      
   } 
+  newPassword = pushChar.join("");
   return newPassword;
   
 }
